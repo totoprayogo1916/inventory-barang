273219ba1992a78ecd15504fc39a4eb0903ade06
Toto <totoprayogo1916@gmail.com>
2022-10-11 14:25:09 +0700

    Penambahan PhpCsFixer
    Co-authored-by: satujalur <98323567+satujalur@users.noreply.github.com>

---
+++ .php-cs-fixer.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use CodeIgniter\CodingStandard\CodeIgniter4;
+use Nexus\CsConfig\Factory;
+use Nexus\CsConfig\FixerGenerator;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__);
+
+$overrides = [
+    'no_extra_blank_lines' => ['tokens' => [
+        'case',
+        'curly_brace_block',
+        'default',
+        'extra',
+        'square_brace_block',
+        'switch',
+        'use',
+    ]],
+];
+
+$options = [
+    'finder'       => $finder,
+    'customFixers' => FixerGenerator::create('vendor/nexusphp/cs-config/src/Fixer', 'Nexus\\CsConfig\\Fixer'),
+];
+
+return Factory::create(new CodeIgniter4(), $overrides, $options)->forProjects();
